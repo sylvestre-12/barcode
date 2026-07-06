20faf2d49119179779e3947e86c77785b2647033
sylvestre-12 <124Tegeri@gmail.com>
2026-07-07 00:11:36 +0200

    professional student ID card PDF system

--- components/StudentQR.tsx
+++ components/StudentQR.tsx
@@ -4,57 +4,48 @@ import { QRCodeSVG } from "qrcode.react";
 
 interface StudentQRProps {
   value: string;
+  size?: number;
 }
-
 
 export default function StudentQR({
   value,
+  size = 100,
 }: StudentQRProps) {
 
-
   return (
-
     <div
       className="
-      flex
-      flex-col
-      items-center
-      justify-center
-      bg-white
-      rounded-lg
-      p-2
+        flex
+        flex-col
+        items-center
+        justify-center
+        bg-white
+        rounded-lg
+        p-1.5
       "
     >
 
       <QRCodeSVG
-
         value={value}
-
-        size={130}
-
+        size={size}
         level="H"
-
-        includeMargin={true}
-
+        includeMargin={false}
+        bgColor="#ffffff"
+        fgColor="#000000"
       />
-
 
       <p
         className="
-        mt-2
-        text-xs
-        font-bold
-        text-gray-700
+          mt-1
+          text-[9px]
+          font-bold
+          text-gray-700
+          tracking-wide
         "
       >
-
         {value}
-
       </p>
 
-
     </div>
-
   );
-
 }
