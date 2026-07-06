33025d9469c76bf421c439d2b8faf06aa0753122
sylvestre-12 <124Tegeri@gmail.com>
2026-07-06 21:23:22 +0200

    prepare for vercel deployment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
